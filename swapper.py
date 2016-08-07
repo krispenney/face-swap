d8c747ace46ef571bf0537a5b4b0dc4c670cf4e6
k9101 <kris9101@gmail.com>
2016-08-07 12:02:47 -0400

    Removed redundant array

--- swapper.py
+++ swapper.py
@@ -1,46 +1,38 @@
 import cv2
 import numpy as np
 
-smile_cascade = cv2.CascadeClassifier('parojos.xml')
+# Load Haar classifer for profile faces
 profile_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 
-while 1:
+while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # smiles = smile_cascade.detectMultiScale(gray, 1.3, 5)
-    smiles = profile_cascade.detectMultiScale(gray, 1.3, 5)
-    faces = []
-    for (x, y, w, h) in smiles:
-    # if(len(smiles) > 0):
-        # (x, y, w, h) = smiles[0]
-        faces.append((x, y, w, h))
-        # sub_face = img[y:y+h, x:x+w]
-        # sub_face = cv2.GaussianBlur(sub_face, (23, 23), 5)
-        # img[y:y+h, x:x+w] = sub_face
-        # cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        # roi_gray = gray[y:y+h, x:x+w]
-        # roi_color = img[y:y+h, x:x+w]
+
+    # detect faces
+    faces = profile_cascade.detectMultiScale(gray, 1.3, 5)
 
     if(len(faces) >= 2):
         (x1, y1, w1, h1) = faces[0]
         (x2, y2, w2, h2) = faces[1]
-        sub_face1 = img[y1:y1+h1, x1:x1+w1]
-        sub_face2 = img[y2:y2+h2, x2:x2+w2]
+
+        roi_face1 = img[y1:y1+h1, x1:x1+w1]
+        roi_face2 = img[y2:y2+h2, x2:x2+w2]
 
         #resize and swap faces
-        sub_face1 = cv2.resize(sub_face1, (h2, w2), interpolation = cv2.INTER_AREA)
-        sub_face2 = cv2.resize(sub_face2, (h1, w1), interpolation = cv2.INTER_AREA)
-        img[y2:y2+h2, x2:x2+w2] = sub_face1
-        img[y1:y1+h1, x1:x1+w1] = sub_face2
+        roi_face1 = cv2.resize(roi_face1, (h2, w2), interpolation = cv2.INTER_AREA)
+        roi_face2 = cv2.resize(roi_face2, (h1, w1), interpolation = cv2.INTER_AREA)
+        img[y2:y2+h2, x2:x2+w2] = roi_face1
+        img[y1:y1+h1, x1:x1+w1] = roi_face2
 
-
+    # output new image
     cv2.imshow('img', img)
-    cv2.waitKey(0)
-    # k = cv2.waitKey(30) & 0xff
-    # if k == 27:
-    #     break
+    # cv2.waitKey(0)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
 
+# Dealloc
 cap.release()
 cv2.destroyAllWindows()
